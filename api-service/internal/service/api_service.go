72ef3075aca95c3549df01199095fc405194b9b7
Artem Kornilaev <artemkornilaev1@gmail.com>
2026-04-23 11:00:57 +0400

    fix: fix api client routes

--- api-service/internal/service/api_service.go
+++ api-service/internal/service/api_service.go
@@ -7,6 +7,8 @@ import (
 	"github.com/keshvan/trod-kafka-lab/api-service/internal/dto"
 )
 
+const dataServiceAPIPrefix = "/api/v1"
+
 type Producer interface {
 	PublishBatch(ctx context.Context, items []dto.AppointmentEvent) error
 }
@@ -32,17 +34,17 @@ func (s *APIService) AddBatch(ctx context.Context, items []dto.AppointmentEvent)
 }
 
 func (s *APIService) SearchAppointments(ctx context.Context, query url.Values) ([]byte, error) {
-	return s.dataClient.Get(ctx, "/appointments", query)
+	return s.dataClient.Get(ctx, dataServiceAPIPrefix+"/appointments", query)
 }
 
 func (s *APIService) GetDailyReport(ctx context.Context, query url.Values) ([]byte, error) {
-	return s.dataClient.Get(ctx, "/reports/daily", query)
+	return s.dataClient.Get(ctx, dataServiceAPIPrefix+"/reports/daily", query)
 }
 
 func (s *APIService) GetTopDoctors(ctx context.Context, query url.Values) ([]byte, error) {
-	return s.dataClient.Get(ctx, "/reports/top-doctors", query)
+	return s.dataClient.Get(ctx, dataServiceAPIPrefix+"/reports/top-doctors", query)
 }
 
 func (s *APIService) GetTopPatients(ctx context.Context, query url.Values) ([]byte, error) {
-	return s.dataClient.Get(ctx, "/reports/top-patients", query)
+	return s.dataClient.Get(ctx, dataServiceAPIPrefix+"/reports/top-patients", query)
 }
